ee85b955e336ec9681ea2b2e51d311b7b6cad455
James Taylor <jamest@uk.ibm.com>
2020-02-27 15:10:50 +0000

    Handle common metadata
    Signed-off-by: James Taylor <jamest@uk.ibm.com>

--- builders/common/utils.sh
+++ builders/common/utils.sh
@@ -36,3 +36,62 @@ function check_chaincode_type {
         error_exit "Unsupported chaincode type: $CHAINCODE_TYPE"
     fi
 }
+
+#######################################
+# Copy the two types of metadata the peer consumes from chaincode source, i.e.
+#   1. state database index definitions for CouchDB
+#   2. external chaincode server connection information
+# Globals:
+#   None
+# Arguments:
+#   FROM_DIR: Location of the metadata
+#   TO_DIR: Destination directory
+# Returns:
+#   None
+#######################################
+function copy_build_metadata_artifacts {
+    local FROM_DIR="$1"
+    local TO_DIR="$2"
+
+    # CouchDB index definitions
+    if [ -d "$FROM_DIR/metadata" ]; then
+        cp -a "$FROM_DIR/metadata" "$TO_DIR/metadata"
+    fi
+
+    # Server connection information
+    if [ -f "$FROM_DIR/connection.json" ]; then
+        cp "$FROM_DIR/connection.json" "$TO_DIR/connection.json"
+        # TODO if tls_required is true, copy TLS files?
+    fi
+}
+
+#######################################
+# Copy the two types of metadata the peer consumes to the required release layout, i.e.
+#   1. state database index definitions for CouchDB
+#   2. external chaincode server connection information
+# Globals:
+#   None
+# Arguments:
+#   FROM_DIR: Location of the metadata
+#   TO_DIR: Destination directory
+# Returns:
+#   None
+#######################################
+function copy_release_metadata_artifacts {
+    local FROM_DIR="$1"
+    local TO_DIR="$2"
+
+    # CouchDB index definitions
+    if [ -d "$FROM_DIR/metadata" ]; then
+        cp -a "$FROM_DIR/metadata/"* "$TO_DIR/"
+    fi
+
+    # Server connection information
+    if [ -f "$FROM_DIR/connection.json" ]; then
+        mkdir -p "$TO_DIR/chaincode/server"
+        cp "$FROM_DIR/connection.json" "$TO_DIR/chaincode/server"
+
+        # TODO if tls_required is true, copy TLS files?
+        # Using above example, the fully qualified path for these fils would be "$RELEASE"/chaincode/server/tls
+    fi
+}
